139325afb64759d9ee5c474e43a4de2639691299
Gino Paul Mendoza Capio <ginocapio@gmail.com>
2016-07-23 22:29:30 -0500

    Add item cost column along with calculation

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-var main = function(){
-  $("#submit").click(function(e){
-    e.preventDefault();
-    var form = $(".form").serializeArray();
-    console.log(form);
-  })
+var resetValues = function(){
+  $(".form").trigger("reset");
+}
 
+var itemCost = function(addItemForm){
+  var cost = addItemForm[1].value * addItemForm[2].value;
+  return cost;
+}
+
+var totalPrice = function(value){
+  var list = $(".listItem")[2].serializeArray();
+  console.log(list);
 }
 
 var addToList = function(){
   $("#submit").click(function(e){
     e.preventDefault();
     var addItemForm = $(".form").serializeArray();
-    $(".total").append("<tr class='listItem'><td>" + addItemForm[0].value + "</td><td>" + addItemForm[1].value +  "</td><td>" + addItemForm[2].value + "</td></tr>");
-  })
+    $(".groceryList").append("<tr class='listItem'><td>" + addItemForm[0].value + "</td><td>" + addItemForm[1].value +  "</td><td>" + addItemForm[2].value + "</td><td>" + itemCost(addItemForm) + "</td></tr>");
+    resetValues();
+    totalPrice();
+  });
+
 };
 
 $(document).ready(addToList);
