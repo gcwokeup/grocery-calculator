bba92d78c37c9ace5a08acab6f6864aecd835574
Gino Paul Mendoza Capio <ginocapio@gmail.com>
2016-07-24 13:32:55 -0500

    Add money formatting to total

--- app.js
+++ app.js
@@ -7,9 +7,33 @@ var itemCost = function(addItemForm){
   return cost;
 }
 
-var totalPrice = function(value){
-  var list = $(".listItem")[2].serializeArray();
-  console.log(list);
+Number.prototype.formatMoney = function(c, d, t){
+var n = this, 
+    c = isNaN(c = Math.abs(c)) ? 2 : c, 
+    d = d == undefined ? "." : d, 
+    t = t == undefined ? "," : t, 
+    s = n < 0 ? "-" : "", 
+    i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
+    j = (j = i.length) > 3 ? j % 3 : 0;
+   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+ };
+
+var totalPrice = function(newValue){
+  var oldTotal = $(".totalPrice").text();
+  var oldVal = parseFloat(oldTotal).toFixed(2);
+  var newVal = parseFloat(newValue).toFixed(2);
+  var total = (parseFloat(newValue) + parseFloat(oldTotal));
+  return "$" + total.formatMoney(2);
+}
+// var totalPrice = function(newValue){
+//   costsArray.push(newValue)
+//   function getSum(total, num) {
+//     return total + num;
+//   return costsArray.reduce(getsum);
+//   }
+// }
+var changeValue = function(selector,newValue){
+  $(selector).text(newValue)
 }
 
 var addToList = function(){
@@ -18,7 +42,9 @@ var addToList = function(){
     var addItemForm = $(".form").serializeArray();
     $(".groceryList").append("<tr class='listItem'><td>" + addItemForm[0].value + "</td><td>" + addItemForm[1].value +  "</td><td>" + addItemForm[2].value + "</td><td>" + itemCost(addItemForm) + "</td></tr>");
     resetValues();
-    totalPrice();
+    console.log(totalPrice(itemCost(addItemForm)));
+    changeValue(".totalPrice", totalPrice(itemCost(addItemForm)))
+
   });
 
 };
